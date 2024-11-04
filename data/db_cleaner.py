2536e8ff7d31b5cc0144b2d76b8cd874a9f292d1
= <samuel.cote13@usherbrooke.ca>
2024-11-04 18:50:51 -0500

    Ajout de colonne enlevée par accident

--- data/db_cleaner.py
+++ data/db_cleaner.py
@@ -8,7 +8,4 @@ df.drop(df[df["isAdult"] == True].index, inplace=True)
 # Garder juste les films
 df.drop(df[df["titleType"] != "movie"].index, inplace=True)
 
-# Supprimer la colonne maintenant inutile
-df.drop("titleType", axis=1, inplace=True)
-
 df.to_csv("./title.basics.tsv", index=False, sep="\t")
