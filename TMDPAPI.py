39eaaf50a1dc5a69d614e7b2bf33136a0b1235c2
Samuel Côté <samuel.cote13@usherbrooke.ca>
2024-11-06 08:53:24 -0500

    Gestion d'erreurs TMDB

--- TMDPAPI.py
+++ TMDPAPI.py
@@ -16,21 +16,32 @@ class TMDB:
         """
         Obtenir la description d'un film à partir de son identifiant unique iMDB
         """
-        resp = requests.get(
-            f"https://api.themoviedb.org/3/find/{imdb_id}?api_key={self.api_key}&external_source=imdb_id")
-        return resp.json()["movie_results"][0]["overview"]
+        try:
+            resp = requests.get(
+                f"https://api.themoviedb.org/3/find/{imdb_id}?api_key={self.api_key}&external_source=imdb_id")
+            return resp.json()["movie_results"][0]["overview"]
+        except Exception:
+            return ""
 
     def get_image(self, imdb_id):
         """
         Obtenir le lien de l'image d'un film à partir de son id iMDB
         """
-        resp = requests.get(
-            f"https://api.themoviedb.org/3/find/{imdb_id}?api_key={self.api_key}&external_source=imdb_id")
-        poster_path = resp.json()["movie_results"][0]["poster_path"]
-        return f"https://image.tmdb.org/t/p/original/{poster_path}"
+        try:
+            resp = requests.get(
+                f"https://api.themoviedb.org/3/find/{imdb_id}?api_key={self.api_key}&external_source=imdb_id")
+            poster_path = resp.json()["movie_results"][0]["poster_path"]
+            return f"https://image.tmdb.org/t/p/original/{poster_path}"
+        except:
+            return None
 
     def __bool__(self):
         """
         Retourne si le programme a pu lire le .env
         """
-        return self.api_key is not None
+        try:
+            resp = requests.get(
+                f"https://api.themoviedb.org/3/authentication?api_key={self.api_key}")
+            return resp.ok
+        except Exception:
+            return False
